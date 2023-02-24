598de7f2f3f439b44191b85559725ad5a59aaffb
Barbika1 <brostas346@gmail.com>
2023-02-24 08:40:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Game from "./Game";
+import "./index.css";
+
+const App = () => {
+  return <div>
+    <Game>
+    </Game>
+    </div>;
+};
+
+export default App;
---
+++ src/FullscreenButton.js
@@ -0,0 +1,14 @@
+import button from "./assets/fullscreen-button.png"
+
+const FullscreenButton = () => {
+
+    const onClick = () => {
+        if (document.fullscreenElement) document.exitFullscreen()
+        else document.documentElement.requestFullscreen()
+    }
+    return <img src={button} id="fullscreen-button" onClick={onClick}/>
+
+
+}
+
+export default FullscreenButton
